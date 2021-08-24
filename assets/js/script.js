80db5d7b41753a9075d9b8bf405e7e9807e3e99c
Diane Galiano <dianegaliano09@gmail.com>
2021-08-24 15:56:47 -0500

    Added current date function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,9 @@
-//Serialize function
-$( function() {
-    $( "#container" ).dayScheduleSelector({
-      stop: function() {
-        var result = $( "#select-result" ).empty();
-        $( ".ui-selected", this ).each(function() {
-          var index = $( "#selectable li" ).index( this );
-          result.append( " #" + ( index + 1 ) );
-        });
-      }
-    });
-  } );
+var timeDisplayEl = $("#currentDay");
 
-  //Date Moment Function
-  var defaultCalendar = {
-    sameDay: '[Today at] LT',
-    nextDay: '[Tomorrow at] LT',
-    nextWeek: 'dddd [at] LT',
-    lastDay: '[Yesterday at] LT',
-    lastWeek: '[Last] dddd [at] LT',
-    sameElse: 'L',
-};
+//Current time and date
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
 
-function calendar(key, mom, now) {
-    var output = this._calendar[key] || this._calendar['sameElse'];
-    return isFunction(output) ? output.call(mom, now) : output;
-}
+setInterval(displayTime, 1000);
