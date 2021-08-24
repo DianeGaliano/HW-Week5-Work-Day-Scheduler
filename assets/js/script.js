498c1197dca70b3795deb10331bb5de979d86a08
Diane Galiano <dianegaliano09@gmail.com>
2021-08-24 13:19:53 -0500

    Fixed jquery link and added selector function to js file.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$( function() {
+    $( "#container" ).dayScheduleSelector({
+      stop: function() {
+        var result = $( "#select-result" ).empty();
+        $( ".ui-selected", this ).each(function() {
+          var index = $( "#selectable li" ).index( this );
+          result.append( " #" + ( index + 1 ) );
+        });
+      }
+    });
+  } );
