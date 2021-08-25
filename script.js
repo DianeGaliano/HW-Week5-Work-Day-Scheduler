e887a93e3b54fdfdbed75fd3f0edb4c95c4ff248
Diane Galiano <dianegaliano09@gmail.com>
2021-08-24 21:53:05 -0500

    Created pseudo code for js scaffolding.

---
+++ script.js
@@ -0,0 +1,12 @@
+//Current Date Displayed at the top of the calendar
+
+//Selection Sterilized Function for calander
+
+//Function to color code time blocks past, present, future
+
+//Click Function within timeblock to add Event
+
+//Click Function to save added event
+
+//Function to save to client local storage.
+
