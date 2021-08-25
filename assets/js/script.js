5050baa99f90742d25479de5874a3dbdba4e71cb
Diane Galiano <dianegaliano09@gmail.com>
2021-08-24 21:10:28 -0500

    Started over

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-var timeDisplayEl = $("#currentDay");
-
-//Current time and date
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
-
-setInterval(displayTime, 1000);
