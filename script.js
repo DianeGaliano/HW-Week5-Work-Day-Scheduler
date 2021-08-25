7d242196a6e7d567b3efae16fd3eca0d37fc34b6
Diane Galiano <dianegaliano09@gmail.com>
2021-08-24 22:02:39 -0500

    Linked js file to html doc. Created current date and time function.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //Current Date Displayed at the top of the calendar
+var timeDisplay = $ ("#currentDay");
 
+function displayTime() {
+    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+    timeDisplay.text(rightNow)
+}
+
+setInterval(displayTime, 1000);
 //Selection Sterilized Function for calander
 
 //Function to color code time blocks past, present, future
