fc057b16b928758e6e8fbbb0288adb404babc802
Diane Galiano <dianegaliano09@gmail.com>
2021-08-27 16:40:40 -0500

    added function to color code time blocks.

--- script.js
+++ script.js
@@ -43,3 +43,19 @@ function saveTextEdit() {
   }
   init();
 
+//Function to test for past, present and future
+var currentHour = (new Date()).getHours();
+
+$('.colorcode')
+  .each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 6){
+      $(this).css('background-color','Blue');
+    }else if(val < currentHour && val > currentHour-6){
+      $(this).css('background-color','Red');
+    }else if(val === currentHour){
+      $(this).css('background-color','Green');
+    }else{
+      $(this).css('background-color','White');
+    }
+  });
