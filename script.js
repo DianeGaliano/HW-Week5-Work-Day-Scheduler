7b6feb2ccc9d0adea977bf355f87f7d8f5553a15
Diane Galiano <dianegaliano09@gmail.com>
2021-08-26 22:57:35 -0500

    Added a local storage function to the save button and had the function display it in the textarea.

--- script.js
+++ script.js
@@ -7,13 +7,39 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
-//Selection Sterilized Function for calander
-
-//Function to color code time blocks past, present, future
-
-//Click Function within timeblock to add Event
-
-//Click Function to save added event
-
-//Function to save to client local storage.
+var comment = document.getElementById("msg");
+var saveButton = document.getElementById("save-button");
+// Function to save to local storage with event listener
+function saveTextEdit() {
+    // Save related form data as an object
+    var editText = {
+      comment: comment.value.trim()
+    };
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem("editText", JSON.stringify(editText));
+  }
+  
+  function renderTextEdit() {
+    // Use JSON.parse() to convert text to JavaScript object
+    var lastTextEdit = JSON.parse(localStorage.getItem("editText"));
+    // Check if data is returned, if not exit out of the function
+    if (lastTextEdit !== null) {
+    document.getElementById("msg").innerHTML = lastTextEdit.comment;
+    } else {
+      return;
+    }
+  }
+  
+  saveButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  saveTextEdit();
+  renderTextEdit();
+  });
+  
+  // The init() function fires when the page is loaded 
+  function init() {
+    // When the init function is executed, the code inside renderLastGrade function will also execute
+    renderTextEdit();
+  }
+  init();
 
