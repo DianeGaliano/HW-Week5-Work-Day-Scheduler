8d9145967a76822a8654f6c559907606662865ac
Diane Galiano <dianegaliano09@gmail.com>
2021-08-28 00:40:00 -0500

    Edit:Color code function. Added div to render color function,

--- script.js
+++ script.js
@@ -7,22 +7,21 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
+
+//Save Button 
 var comment = document.getElementById("msg");
 var saveButton = document.getElementById("save-button");
+
 // Function to save to local storage with event listener
 function saveTextEdit() {
-    // Save related form data as an object
     var editText = {
       comment: comment.value.trim()
     };
-    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
     localStorage.setItem("editText", JSON.stringify(editText));
   }
   
   function renderTextEdit() {
-    // Use JSON.parse() to convert text to JavaScript object
     var lastTextEdit = JSON.parse(localStorage.getItem("editText"));
-    // Check if data is returned, if not exit out of the function
     if (lastTextEdit !== null) {
     document.getElementById("msg").innerHTML = lastTextEdit.comment;
     } else {
@@ -36,24 +35,26 @@ function saveTextEdit() {
   renderTextEdit();
   });
   
-  // The init() function fires when the page is loaded 
   function init() {
-    // When the init function is executed, the code inside renderLastGrade function will also execute
     renderTextEdit();
   }
   init();
 
+
 //Function to test for past, present and future
+
 var currentHour = (new Date()).getHours();
 
-$(".time-block")
-  .each(function(){
-    var val = parseInt($(this).prop('id'));
+$(".colorcode").each(function(){
+    var val = parseInt($(this).prop("id"));
     if(val > currentHour && val < currentHour + 6){
-      $(this).css(".future");
-    }else if(val < currentHour && val > currentHour-6){
-      $(this).css(".past");
+      //Future
+      $(this).css("background-color: #77dd77", "color: white");
     }else if(val === currentHour){
-      $(this).css(".present");
+      //Current
+      $(this).css("background-color: #ff6961", "color: white");
+    }else{
+      //Past
+      $(this).css("background-color: #d3d3d3", "color: white");
     }
   });
