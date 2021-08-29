18ee2edb073775b771217a3c3dec243e2502e151
Diane Galiano <dianegaliano09@gmail.com>
2021-08-29 03:00:37 -0500

    fixed text save function.

--- script.js
+++ script.js
@@ -8,37 +8,6 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
-//Save Button 
-var comment = document.getElementById("msg")
-var saveButton = document.getElementById("save-button")
-
-var messageInput = document.querySelector("#msg")
-
-function saveText() {
-  // Save related form data as an object
-  var textEdit = {
-  comment: comment.value.trim()
-  };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("textEdit", JSON.stringify(textEdit));
-}
-
-saveButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  renderText();
-  saveText();
-});
-
-function init() {
-  var lastEdit = JSON.parse(localStorage.getItem("textEdit"));
-   if (lastEdit !== null) {
-    document.getElementById("msg").innerHTML = lastEdit.comment;
-  }else {
-  return;
-  } 
-  renderText();
-}
-
 //Color
 $(".colorCode").each(function(){
   var time = parseInt($(this).prop("id"));
@@ -50,3 +19,43 @@ $(".colorCode").each(function(){
     $(this).addClass("future");
   }
 });
+//Save Button 
+var comment = document.getElementById("msg")
+var saveButton = document.getElementById("save-button")
+
+
+
+
+function saveText() {
+  // Save related form data as an object
+  var textEdit = {
+  comment: comment.value.trim()
+  };
+  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+  localStorage.setItem("textEdit", JSON.stringify(textEdit));
+}
+function renderText() {
+  // Use JSON.parse() to convert text to JavaScript object
+  var lastEdit = JSON.parse(localStorage.getItem("textEdit"));
+  // Check if data is returned, if not exit out of the function
+  if (lastEdit !== null) {
+  document.getElementById("msg").innerHTML = lastEdit.comment;
+  } else {
+    return;
+  }
+}
+
+saveButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  renderText();
+  saveText();
+});
+
+function init() {
+  // When the init function is executed, the code inside renderLastGrade function will also execute
+  renderText();
+}
+init();
+
+
+
