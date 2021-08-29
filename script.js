fb557e02a6203e74616ce3e190ab94a893e2d613
Diane Galiano <dianegaliano09@gmail.com>
2021-08-29 09:57:50 -0500

    fixed color code issue.

--- script.js
+++ script.js
@@ -9,53 +9,21 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 //Color
+var currentHour = (new Date()).getHours();
+
 $(".colorCode").each(function(){
   var time = parseInt($(this).prop("id"));
-  if (time === timeDisplay){
+  if (time === currentHour){
     $(this).addClass("present");
-  }else if(time < timeDisplay){
+  }else if(time < currentHour){
     $(this).addClass("past");
   }else{
     $(this).addClass("future");
   }
 });
-//Save Button 
-var comment = document.getElementById("msg")
-var saveButton = document.getElementById("save-button")
 
 
 
-
-function saveText() {
-  // Save related form data as an object
-  var textEdit = {
-  comment: comment.value.trim()
-  };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("textEdit", JSON.stringify(textEdit));
-}
-function renderText() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var lastEdit = JSON.parse(localStorage.getItem("textEdit"));
-  // Check if data is returned, if not exit out of the function
-  if (lastEdit !== null) {
-  document.getElementById("msg").innerHTML = lastEdit.comment;
-  } else {
-    return;
-  }
-}
-
-saveButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  renderText();
-  saveText();
-});
-
-function init() {
-  // When the init function is executed, the code inside renderLastGrade function will also execute
-  renderText();
-}
-init();
 
 
 
